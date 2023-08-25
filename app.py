9cced16ecd115a040150681ce98fb9833e6dcf06
Sarim Khan <sarimkhanskd@gmail.com>
2023-08-24 21:34:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from keras.models import model_from_json
 from keras.preprocessing.image import img_to_array
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
 
+# set page title
+st.set_page_config(page_title="Face Analysis")
+
 # load model
 emotion_dict = {0:'angry', 1 :'happy', 2: 'neutral', 3:'sad', 4: 'surprise'}
 # load json and create model
